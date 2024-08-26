24f8ec8f5936964a666f5bc132a46420639f1ca6
aryan42bit <aryankulkarni6969@gmail.com>
2024-08-26 21:51:04 +0530

    Create even_number.cpp

---
+++ even_number.cpp
@@ -0,0 +1,28 @@
+//23070123086
+//aryan kulkarni//
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i=0;i<=10;i++)
+    {
+        if(i==3||i==5)
+        {
+            continue;
+           
+        }
+        cout<<i<<endl;
+    }
+}
+
+/*
+0
+1
+2
+4
+6
+7
+8
+9
+10
+*/
